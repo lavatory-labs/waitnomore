55700df2977f238497f9ec09c2a2bc02b23f9931
Tyler Nappy <tylernappy@gmail.com>
2017-04-26 16:55:06 -0400

    first try

--- index.py
+++ index.py
@@ -1,5 +1,46 @@
 import picamera
 
+from colorthief import ColorThief
 
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+
+from time import sleep
+
+file_name = "image_temp.png"
+
+sleep(2)
 while True:
-    print "hello!"
+    ## take picture
+    take_picture()
+
+    ## Determine if red or green
+    dominate_color = check_dominate_color()
+    print dominate_color
+
+    ## make request to server if red or green
+
+def take_picture():
+    print "Taking picture..."
+    camera.capture(file_name)
+
+
+def check_dominate_color():
+    color_thief = ColorThief(file_name)
+    # get the dominant color
+    dominant_color = color_thief.get_color(quality=10)
+    print dominant_color
+    r, g, b = dominant_color
+
+    print dominant_color
+    if r > g:
+        return "red"
+    elif g > r:
+        return "green"
+    else:
+        return "error"
+
+    # build a color palette
+    #palette = color_thief.get_palette(color_count=6)
+    #print palette
