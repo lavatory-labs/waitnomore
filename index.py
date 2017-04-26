1106554c85aa9528df509969c4ded6dba4ef6e28
Tyler Nappy <tylernappy@gmail.com>
2017-04-26 17:30:28 -0400

    added request to server

--- index.py
+++ index.py
@@ -2,6 +2,8 @@ import picamera
 
 from colorthief import ColorThief
 
+import requests
+
 camera = picamera.PiCamera()
 camera.resolution = (1024, 768)
 camera.start_preview()
@@ -9,6 +11,8 @@ camera.start_preview()
 from time import sleep
 
 file_name = "image_temp.png"
+
+server_url = "http://requestb.in/1a52gtf1"
 
 def take_picture():
     print "Taking picture..."
@@ -22,7 +26,6 @@ def check_dominate_color():
     print dominant_color
     r, g, b = dominant_color
 
-    print dominant_color
     if r > g:
         return "red"
     elif g > r:
@@ -34,13 +37,21 @@ def check_dominate_color():
     #palette = color_thief.get_palette(color_count=6)
     #print palette
 
+def check_dominate_color(dominate_color):
+    r = requests.get(server_url, params={"color": dominate_color} )
+    print r.status_code
+    print r.content
+
 sleep(2)
 while True:
-    ## take picture
+    ## Take picture
     take_picture()
 
     ## Determine if red or green
     dominate_color = check_dominate_color()
-    print dominate_color
 
-    ## make request to server if red or green
+    ## Make request to server if red or green
+    try:
+        report_status(dominate_color)
+    except Exception:
+        pass
