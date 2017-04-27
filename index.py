d81b8ff47f4715b5de94aebf211cc17a6a4f43f0
Tyler Nappy <tylernappy@gmail.com>
2017-04-27 16:04:55 -0400

    updated logic and changed to post request

--- index.py
+++ index.py
@@ -5,7 +5,8 @@ from colorthief import ColorThief
 import requests
 
 camera = picamera.PiCamera()
-camera.resolution = (1024, 768)
+#camera.resolution = (1024, 768)
+camera.resolution = (640, 480)
 camera.start_preview()
 
 from time import sleep
@@ -19,27 +20,30 @@ def take_picture():
     camera.capture(file_name)
 
 def check_dominate_color():
+    print "capturing dominant color"
     color_thief = ColorThief(file_name)
     # get the dominant color
     dominant_color = color_thief.get_color(quality=10)
     print dominant_color
     r, g, b = dominant_color
 
-    if r > g:
+    if r > 100:
+        return "occupied"
+    return "available"
+
+    if r > g and r > b:
         ## color is red
         return "occupied"
-    elif g > r:
-        ## color is green
-        return "available"
-    else:
-        return "error"
+
+    return "available"
 
     # build a color palette
     #palette = color_thief.get_palette(color_count=6)
     #print palette
 
 def report_status(dominate_color):
-    r = requests.get(server_url, params={"color": dominate_color} )
+    print "reporting status: {}".format(dominate_color)
+    r = requests.post(server_url, json={"status": dominate_color} )
     print r.status_code
     print r.content
 
@@ -54,5 +58,6 @@ while True:
     ## Make request to server if red or green
     try:
         report_status(dominate_color)
-    except Exception:
+    except Exception as e:
+        print e
         pass
